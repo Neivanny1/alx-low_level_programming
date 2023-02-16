8b03c34d51537f8f2cd98fafdcc5255bac9bc9f9
Neivanny1 <nevileneivanny@gmail.com>
2023-02-16 05:08:39 -0800

    Task 5.Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ * Return: Always returns (0)
+ * This programs oupts the string without warning while using -wall gcc
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
