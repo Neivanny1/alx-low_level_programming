f802f8b1ef686b7569dd34863bf5bbcb4731618d
Neivanny1 <nevileneivanny@gmail.com>
2023-02-21 15:34:49 -0800

    10 betty error solved

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,15 +2,15 @@
 
 /**
  * add - adds two integers and returns the result
- * @num1: int to be added to num2
- * @num2: int to be added to num1
- * Return: sum of num1 and num2
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
-int add(int num1, int num2)
+int add(int a, int b)
 {
-        int result;
+	int c;
 
-        result = num1 + num2;
+	c = a + b;
 
-        return (result);
+	return (c);
 }
