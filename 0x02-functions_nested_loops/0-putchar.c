d97d78bfc3431172650abbe5f628406db415c4a2
Neivanny1 <nevileneivanny@gmail.com>
2023-02-20 23:16:09 -0800

    Task 0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints _putchar
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains all projects for ALX 0x02. C - Functions, nested loops project
