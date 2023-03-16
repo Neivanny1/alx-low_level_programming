85d9fdf37a6ce50dd2ec1d5e874e0cc85ee82c8e
Neivanny1 <nevileneivanny@gmail.com>
2023-03-16 05:20:33 -0700

    Program has a function that reallocates a memory block using malloc, free and  Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size); where ptr is a pointer to the memory previously allocated with a call to malloc: malloc(old_size) old_size is the size, in bytes, of the allocated space for ptr and new_size is the new size, in bytes of the new memory block. The contents will be copied to the newly allocated space, in the range from the start of ptr up to the minimum of the old and new sizes. If new_size > old_size, the “added” memory should not be initialized. If new_size == old_size do not do anything and return ptr. If ptr is NULL, then the call is equivalent to malloc(new_size), for all values of old_size and new_size. If new_size is equal to zero, and ptr is not NULL, then the call is equivalent to free(ptr). Return NULL and free ptr when it makes sense

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated with a call to malloc.
+ * @old_size: The size, in bytes, of the allocated space for ptr.
+ * @new_size: The new size, in bytes, of the new memory block.
+ *
+ * Return: A pointer to the reallocated memory block.
+ *         If new_size == old_size, return ptr.
+ *         If new_size == 0 and ptr is not NULL, return NULL and free ptr.
+ *         If malloc fails, return NULL and leave ptr untouched.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr, *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+
+		return (new_ptr);
+	}
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+
+	old_ptr = ptr;
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_ptr[i] = old_ptr[i];
+
+	free(ptr);
+
+	return (new_ptr);
+}
