856374e7b04900492237be573c54efb47a2fe42b
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-04-10 20:58:23 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,32 +10,32 @@
 
 int main(int holder1, char *holder2[])
 {
-	int main_file, copy_file, read_counts, write_counts;
+	int file_from, file_to, read_counts, write_counts;
 	char buffer[BUFF_SIZE];
 
 	if (holder1 != 3)
-		dprintf(STDERR_FILENO, "Usage: cp main_file copy_file\n"), exit(97);
-	main_file = open(holder2[1], O_RDONLY);
-	if (main_file == -1)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = open(holder2[1], O_RDONLY);
+	if (file_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", holder2[1]), exit(98);
 
-	copy_file = open(holder2[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (copy_file == -1)
+	file_to = open(holder2[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", holder2[2]), exit(99);
 
 	do {
-		read_counts = read(main_file, buffer, BUFF_SIZE);
+		read_counts = read(file_from, buffer, BUFF_SIZE);
 		if (read_counts == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", holder2[1]), exit(98);
-		write_counts = write(copy_file, buffer, read_counts);
+		write_counts = write(file_to, buffer, read_counts);
 		if (write_counts == -1)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", holder2[2]), exit(99);
 	} while (read_counts == BUFF_SIZE);
 
-	if (close(main_file) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", main_file), exit(100);
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 
-	if (close(copy_file) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copy_file), exit(100);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
