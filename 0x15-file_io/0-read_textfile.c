01a423d3cdb4d3f5b83ecb48cfddbb00a43d5be1
Neivanny1 <nevileneivanny@gmail.com>
2023-04-10 04:51:19 -0700

    Has a function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,45 +5,42 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/**
- * read_textfile - reads a textfile and prints it to POSIX output
- * @filename: Points to location of file
- * @letters: number of letters to read and print
- *
- * Return: 0 if file can't be opened, read or write fails
- */
+**
+* read_textfile - reads a textfile and prints it to POSIX output
+* @filename: Points to location of file
+* @letters: number of letters to read and print
+*
+* Return: 0 if file cant be opened, read, or write fails
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int opened_File, read_Bytes;
-        char *buffer;
+	int opened_File, read_Bytes;
+	char *buffer;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(letters * sizeof(char));
-        if (buffer == NULL)
-                return (0);
+opened_File = open(filename, O_RDONLY);
+if (opened_File == -1)
+return (0);
 
-        opened_File = open(filename, O_RDONLY);
-        if (opened_File == -1)
-        {
-                free(buffer);
-                return (0);
-        }
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
 
-        read_Bytes = read(opened_File, buffer, letters);
-        if (read_Bytes == -1)
-        {
-                free(buffer);
-                close(opened_File);
-                return (0);
-        }
+read_Bytes = read(opened_File, buffer, letters);
+if (read_Bytes == -1)
+{
+free(buffer);
+return (0);
+}
 
-        write(STDOUT_FILENO, buffer, read_Bytes);
+write(STDOUT_FILENO, buffer, read_Bytes);
 
-        free(buffer);
-        close(opened_File);
+free(buffer);
+close(opened_File);
 
-        return (read_Bytes);
+return (read_Bytes);
 }
 
