dc9f876a727d41d199bec96e3bd66fc47451b15e
Neivanny1 <nevileneivanny@gmail.com>
2023-04-10 05:40:49 -0700

    function that creates a file

--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,2 +1,3 @@
 0x15. C - File I/O
 Task0 -  function that reads a text file and prints it to the POSIX standard output.
+Task1 - function that creates a file
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,4 +7,5 @@
 #include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
