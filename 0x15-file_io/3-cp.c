0de9f71b2bd0b4e07cb44b560cf5a8cdeb0f372c
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-04-10 19:26:23 +0300

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,39 +10,49 @@
 
 int main(int holder1, char *holder2[])
 {
-	int main_file, copy_file, read_counts, write_counts;
-	char buffer[BUFF_SIZE];
+	int main_file, copy_file, read_counts;
+	char buf[BUFFER_SIZE];
 
 	if (holder1 != 3)
-		dprintf(STDERR_FILENO, "Usage: cp main_file copy_file\n");
+	{
+		dprintf(STDERR_FILENO, "Usage: %s main_file copy_file", holder2[0]);
 		exit(97);
+	}
 	main_file = open(holder2[1], O_RDONLY);
 	if (main_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", holder2[1]);
+		exit(98);
+	}
+	copy_file = open(holder2[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (copy_file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", holder2[2]);
+		exit(99);
+	}
+
+	while ((read_count = read(main_file, buf, BUFFER_SIZE)) > 0)
+	{
+		if (write(copy_file, buf, read_count) != read_count)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", holder2[2]);
+			exit(99);
+		}
+	}
+	if (read_count == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", holder2[1]);
 		exit(98);
-
-	copy_file = open(holder2[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (copy_file == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", holder2[2]);
-		exit(99);
-
-	do {
-		read_counts = read(main_file, buffer, BUFF_SIZE);
-		if (read_counts == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", holder2[1]);
-			exit(98);
-		write_counts = write(copy_file, buffer, read_counts);
-		if (write_counts == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", holder2[2]);
-			exit(99);
-	} while (read_counts == BUFF_SIZE);
-
+	}
 	if (close(main_file) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", main_file);
 		exit(100);
-
+	}
 	if (close(copy_file) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copy_file);
 		exit(100);
+	}
 	return (0);
 }
