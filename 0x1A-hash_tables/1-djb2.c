76886f6d12cdd7ab7f3dac7507417cd81376133f
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2023-09-08 14:40:09 -0400

    Update 1-djb2.c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -12,6 +12,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5)) + c; /* hash * 33 + c */
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
 	return (hash);
 }
