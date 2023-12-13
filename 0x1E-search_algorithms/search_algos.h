75eee1f81363ecee450c6a2fed5d2054d22d5f41
Neivanny1 <nevileneivanny@gmail.com>
2023-12-13 18:56:48 +0000

    added missing library

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define SEARCH_ALGOS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t position, size_t size);
